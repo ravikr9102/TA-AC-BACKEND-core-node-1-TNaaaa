d86e2b8d03fc7f26cc70ac0d23d5d61b276eaf11
Ravi kumar <rkravikr9102@gmail.com>
2022-07-04 13:05:50 +0530

    Done Assignment

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,17 @@
+function square(a){
+    return a * a;
+}
+
+function rectangle (a,b){
+    return a * b;
+}
+
+function circle(r){
+    return 2 * 3.14 * r;
+}
+
+module.exports = {
+    square: square,
+    rectangle: rectangle,
+    circle: circle
+}
